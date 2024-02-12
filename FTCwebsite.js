21d9d86756d9ee92b7db2ce6f8a046e9728b7a4f
KingJellofish <159583628+KingJellofish@users.noreply.github.com>
2024-02-12 09:06:46 -0800

    Update FTCwebsite.js

--- FTCwebsite.js
+++ FTCwebsite.js
@@ -1,17 +1,31 @@
+window.scrollTo(0, 0);
+
 let btn = document.querySelector('#btn')
 let sidebar = document.querySelector ('.sidebar');
 
 btn.onclick = function () {
     sidebar.classList.toggle('active');
+    if (sidebar.classList.contains("active")) {
+        retract();
+        menu.style.display = "none";
+        fixTransitions("0.5s ease-in");
+        delay(navItems);
+    } else {
+        menu.style.display = "block";
+        dropped = false;
+        fixTransitions("0.2s");
+    }
 };
 
-let courses = document.querySelector("#dropdown");
-let dropped = true;
+let courses = document.querySelector(".dropdown");
+let menu = document.querySelector(".dropdown-menu");
+let dropped = false;
 function toggleDropdown() {
+    menu.style.display = "block";
     if (dropped) {
-        drop();
-    } else {
         retract();
+    } else {
+        drop();
     }
     dropped = !dropped;
 }
@@ -23,6 +37,20 @@ function drop() {
 function retract() {
     courses.classList.add("hidden");
     courses.classList.remove("displayed");
+}
+
+let navItems = document.querySelectorAll(".nav-item");
+function fixTransitions(value) {
+    for (var i = 0; i < navItems.length; i++) {
+        navItems[i].style.transition = value;
+    }
+}
+function delay(array) {
+    window.setTimeout(function() {
+        for (var i = 0; i < array.length; i++) {
+            array[i].style.transition = "0.2s";
+        }
+    }, 500);
 }
 
 const parallax = document.getElementById("parallax");
