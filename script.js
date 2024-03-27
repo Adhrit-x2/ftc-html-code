bc4a92b2c5fd10eec88c5f87cd48606a55fa2b89
KingJellofish <159583628+KingJellofish@users.noreply.github.com>
2024-03-27 11:12:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -150,3 +150,20 @@ function adjustSize() {
 
   // Re-adjust on window resize
   window.addEventListener('resize', adjustSize);
+
+var listHeaders = document.getElementsByClassName("list-header");
+var footerDroppers = document.getElementsByClassName("footer-dropdown");
+
+var toggles = [false, false, false, false];
+function toggleDropdown(index) {
+    if (toggles[index]) {
+        //footerDroppers[index].style.animation = "footer-dropup 0.5s 1 forwards";
+        footerDroppers[index].style.height = 0;
+    } else {
+        //footerDroppers[index].style.animation = "footer-dropdown 0.5s 1 forwards";
+        footerDroppers[index].style.height = index === 1 ? "150px" : "100px";
+    }
+    toggles[index] = !toggles[index];
+}
+
+
