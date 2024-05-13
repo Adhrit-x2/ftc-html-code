00194463999cf8e9b921a65df9913924cc7e6fcb
KingJellofish <159583628+KingJellofish@users.noreply.github.com>
2024-05-13 13:51:59 -0700

    Update ourteam.js

--- ourteam.js
+++ ourteam.js
@@ -1,1 +1,29 @@
 window.scrollTo(0, 0);
+
+var people = document.querySelectorAll(".person h2")
+var peopleSections = document.querySelectorAll(".person-expand");
+var bios = document.querySelectorAll(".bio");
+peopleSections.forEach((element, index) => {
+    var on = false;
+    people[index].addEventListener("click", () => {
+        element.style.height = on ? 0 : (bios[index].getBoundingClientRect().height + 20).toString() + "px";
+        on = !on;
+    });
+});
+peopleSections[peopleSections.length - 1].style.border = "none";
+
+var listHeaders = document.getElementsByClassName("list-header");
+var footerDroppers = document.getElementsByClassName("footer-dropdown");
+
+var toggles = [false, false, false, false];
+function toggleDropdown(index) {
+    if (toggles[index]) {
+        //footerDroppers[index].style.animation = "footer-dropup 0.5s 1 forwards";
+        footerDroppers[index].style.height = 0;
+    } else {
+        //footerDroppers[index].style.animation = "footer-dropdown 0.5s 1 forwards";
+        footerDroppers[index].style.height = index === 1 ? "150px" : "100px";
+    }
+    toggles[index] = !toggles[index];
+}
+
