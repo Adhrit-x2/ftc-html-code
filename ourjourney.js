cce9c7056ab5adbe48d53e7ccf4f895ec647ef29
KingJellofish <159583628+KingJellofish@users.noreply.github.com>
2024-05-13 13:50:42 -0700

    Update ourjourney.js

--- ourjourney.js
+++ ourjourney.js
@@ -1,1 +1,14 @@
-var yes = "yes";
+var listHeaders = document.getElementsByClassName("list-header");
+var footerDroppers = document.getElementsByClassName("footer-dropdown");
+
+var toggles = [false, false, false, false];
+function toggleDropdown(index) {
+    if (toggles[index]) {
+        //footerDroppers[index].style.animation = "footer-dropup 0.5s 1 forwards";
+        footerDroppers[index].style.height = 0;
+    } else {
+        //footerDroppers[index].style.animation = "footer-dropdown 0.5s 1 forwards";
+        footerDroppers[index].style.height = index === 1 ? "150px" : "100px";
+    }
+    toggles[index] = !toggles[index];
+}
